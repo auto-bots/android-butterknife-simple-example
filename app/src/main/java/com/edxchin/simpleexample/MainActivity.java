fe6751ec2cee476a530e30bcad1a7f28c379ffc2
auto-bots <auto-bots@users.noreply.github.com>
2016-01-04 20:20:21 -0800

    initial commit

---
+++ app/src/main/java/com/edxchin/simpleexample/MainActivity.java
@@ -0,0 +1,23 @@
+package com.edxchin.simpleexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.textView)
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        textView.append("_added at runtime");
+    }
+}
